f1cddb112edadd3ae2275d9fd22360359d7ea48b
Mark Turney <markturney@gmail.com>
2012-06-02 01:38:54 -0700

    File menu, icons, and a fixed FileSystemWatcher.

---
+++ source/filesystemwatcher.cpp
@@ -0,0 +1,62 @@
+
+#include "filesystemwatcher.h"
+
+#include <QTimer>
+
+#include <algorithm>
+
+FileSystemWatcher::FileSystemWatcher(QObject * parent) :
+    QFileSystemWatcher(parent), deleted_files_timer(new QTimer(this)),
+    file_info(new QFileInfo())
+{
+    file_info->setCaching(false);
+
+    connect(this, SIGNAL(fileChanged(QString)), SLOT(slotFileChanged(QString)));
+    connect(deleted_files_timer, SIGNAL(timeout()), SLOT(slotExamineDeletedFiles()));
+}
+
+void FileSystemWatcher::slotFileChanged(QString file_uri)
+{
+    QFileInfo file_info(file_uri);
+    if (!file_info.exists())
+    {
+        if (deleted_files.empty())
+            deleted_files_timer->start(500);
+
+        deleted_files.push_back(file_uri);
+        removePath(file_uri);
+    }
+}
+
+namespace
+{
+    struct OnDisk
+    {
+        OnDisk(QFileInfo * file_info) : file_info(file_info) { }
+        bool operator () (QString const & file_uri)
+        {
+            file_info->setFile(file_uri);
+            return file_info->exists();
+        }
+        QFileInfo * file_info;
+    };
+}
+
+void FileSystemWatcher::slotExamineDeletedFiles()
+{
+    // If any of the deleted files have been restored, start watching them again.
+    for (std::size_t i = 0; i < deleted_files.size(); ++i)
+    {
+        QString file_uri = deleted_files[i];
+        file_info->setFile(file_uri);
+        if (file_info->exists())
+            addPath(file_uri);
+    }
+
+    deleted_files.erase(
+        std::remove_if(deleted_files.begin(), deleted_files.end(), OnDisk(file_info)),
+        deleted_files.end());
+
+    if (deleted_files.empty())
+        deleted_files_timer->stop();
+}
---
+++ source/filesystemwatcher.hpp
@@ -0,0 +1,31 @@
+
+#ifndef FILE_SYSTEM_WATCHER_H
+#define FILE_SYSTEM_WATCHER_H
+
+#include <QFileSystemWatcher>
+#include <QFileInfo>
+
+#include <vector>
+
+class QTimer;
+class QFileInfo;
+
+// This class keeps track of deleted files and if these deleted
+//  files are restored, it starts to watch them once again.
+class FileSystemWatcher : public QFileSystemWatcher
+{
+    Q_OBJECT
+public:
+    explicit FileSystemWatcher(QObject * parent = 0);
+
+private slots:
+    void slotFileChanged(QString);
+    void slotExamineDeletedFiles();
+
+private:
+    QTimer * deleted_files_timer;
+    std::vector<QString> deleted_files;
+    QFileInfo * file_info;
+};
+
+#endif
